34249cacca70e24de52ac3625fd9e8a141951de3
S564568 <S564568@nwmissouri.edu>
2023-11-10 10:49:58 -0600

    lab 12 update

--- controllers/horse.js
+++ controllers/horse.js
@@ -3,7 +3,7 @@ var horse = require('../models/horse');
 
 
 
-// List of all Costumes
+// List of all horses
 exports.horse_list = async function(req, res) {
     try{
     thehorses= await horse.find();
@@ -48,7 +48,7 @@ let document = new horse();
 // We are looking for a body, since POST does not have query parameters.
 // Even though bodies can be in many different formats, we will be picky
 // and require that it be a json object
-// {"costume_type":"goat", "cost":12, "size":"large"}
+// {"horse_type":"goat", "cost":12, "size":"large"}
 
 document.name = req.body.name;
 document.color = req.body.color;
@@ -63,6 +63,17 @@ res.send(`{"error": ${err}}`);
 }
 };
 
+// for a specific horse.
+exports.horse_detail = async function(req, res) {
+console.log("detail" + req.params.id)
+try {
+result = await horse.findById( req.params.id)
+res.send(result)
+} catch (error) {
+res.status(500)
+res.send(`{"error": document for id ${req.params.id} not found`);
+}
+};
 
 
 
@@ -71,7 +82,28 @@ res.send(`{"error": ${err}}`);
 exports.horse_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: horse delete DELETE ' + req.params.id);
 };
-// Handle horse update form on PUT.
-exports.horse_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: horse update PUT' + req.params.id);
-};
+
+exports.horse_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await horse.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.horse_name)
+    toUpdate.horse_name = req.body.horse_name;
+    if(req.body.color) toUpdate.color = req.body.color;
+    if(req.body.price) toUpdate.price = req.body.price;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+
+
+
+
+
