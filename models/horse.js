8426e806788bd073932666ce651fc6a8924f23a0
S564568 <S564568@nwmissouri.edu>
2023-11-29 21:01:46 -0600

    part 4 and 5 commit and push

--- models/horse.js
+++ models/horse.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const horseSchema = mongoose.Schema({
 name: String,
-color: String,
-price: Number
+color: {type:String,minlength:2,maxlength:44444},
+price: {type:Number,min:4,max:4444444}
 })
 module.exports = mongoose.model("horse",horseSchema)
--- routes/horse.js
+++ routes/horse.js
@@ -4,12 +4,20 @@ var router = express.Router();
 /* GET costumes */
 router.get('/', horse_controlers.horse_view_all_Page );
 module.exports = router;
+// A little function to check if we have an authorized user and continue on
 
-router.get('/detail', horse_controlers.horse_view_one_Page);
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ re
 
-router.get('/create', horse_controlers.horse_create_Page);
+router.get('/detail',secured, horse_controlers.horse_view_one_Page);
 
-router.get('/update', horse_controlers.horse_update_Page);
+router.get('/create',secured, horse_controlers.horse_create_Page);
 
-router.get('/delete', horse_controlers.horse_delete_Page);
+router.get('/update',secured, horse_controlers.horse_update_Page);
+
+router.get('/delete',secured, horse_controlers.horse_delete_Page);
 
